e20f4d7889b3b2f78e4afa9c340edb983f062995
bwhite04 <60192676+bwhite04@users.noreply.github.com>
2023-03-20 03:25:54 +0200

    Update wp-config.php
    pass base

--- mr/wp-config.php
+++ mr/wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'mr');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'password');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
